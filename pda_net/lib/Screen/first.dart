509f75a97ae44aacafc794cd683f2e4af00c8716
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-08-22 21:01:58 +0200

    Some updates in my application

---
+++ pda_net/lib/Screen/first.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class firstScreen extends StatefulWidget {
+  const firstScreen({super.key});
+
+  @override
+  State<firstScreen> createState() => _firstScreenState();
+}
+
+class _firstScreenState extends State<firstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
