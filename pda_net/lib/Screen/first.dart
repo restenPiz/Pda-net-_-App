8340147c6c2c0822aca86ac0e540444ce2552010
Mauro Peniel <99766054+restenPiz@users.noreply.github.com>
2023-08-24 12:20:31 +0200

    Some updates in my firstScreen

--- pda_net/lib/Screen/first.dart
+++ pda_net/lib/Screen/first.dart
@@ -14,6 +14,7 @@ class _firstScreenState extends State<firstScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      //?Inicio do meu appBar 
       appBar: AppBar(
         title: Text('PDA NET +'),
         backgroundColor: Colors.black,
